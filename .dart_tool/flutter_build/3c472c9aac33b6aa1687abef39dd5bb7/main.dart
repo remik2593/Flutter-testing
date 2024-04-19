48965e7e6673259e9cc9eb4aebaa916866ffef0d
remik2593 <>
2024-04-19 10:53:14 +0200

    Testowy commit

---
+++ .dart_tool/flutter_build/3c472c9aac33b6aa1687abef39dd5bb7/main.dart
@@ -0,0 +1,30 @@
+// @dart=2.19
+// Flutter web bootstrap script for file:///Users/remikgagala/Desktop/Automaty/Testing_in_flutter_Kodecco/test/test_with_mock_data.dart.
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: type=lint
+
+import 'dart:ui_web' as ui_web;
+import 'dart:async';
+
+import 'file:///Users/remikgagala/Desktop/Automaty/Testing_in_flutter_Kodecco/test/test_with_mock_data.dart' as entrypoint;
+import 'web_plugin_registrant.dart' as pluginRegistrant;
+
+typedef _UnaryFunction = dynamic Function(List<String> args);
+typedef _NullaryFunction = dynamic Function();
+
+Future<void> main() async {
+  await ui_web.bootstrapEngine(
+    runApp: () {
+      if (entrypoint.main is _UnaryFunction) {
+        return (entrypoint.main as _UnaryFunction)(<String>[]);
+      }
+      return (entrypoint.main as _NullaryFunction)();
+    },
+    registerPlugins: () {
+      pluginRegistrant.registerPlugins();
+    },
+  );
+}
---
+++ test/Unit test/test_with_mock_data.dart
@@ -0,0 +1,80 @@
+import 'package:Testing/models/quotes.dart';
+import 'package:Testing/notifiers/quotes_notifier.dart';
+import 'package:Testing/services/quotes_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/src/client.dart';
+import 'package:mocktail/mocktail.dart';
+
+/* Klasa symulowana MockQuotesService z wykorzystaniem paczki Mocktail, 
+która sprawia, ze nie musimy implementować wszystkich metod klas */
+class MockQuotesService extends Mock implements QuotesService {}
+
+void main() {
+//sut (System Under Test) oznacza, ze testy pisane są dla klasy QuotesNotifier więc klasa ta jest systemem poddanym testowi
+  late QuotesNotifier sut_quotesNotifier;
+/* Tworzymy zmienną klasy MockQuotesService bez zmiennej sut, poniewaz nie tworzymy
+systemu poddawanego testowi tylko tworzymy klasę symulowaną */
+  late MockQuotesService mockQuotesService;
+
+/* Funkcja setUp jest wywoływana przed kazdym testem, mozemy napisać wspólny kod
+wewnątrz funkcji i zainicjować zmienne, które będą uzywane w teście */
+  setUp(() {
+// Tworzymy obiekt klasy MockQuotesService i przekazujemy go do konstruktora QuotesNotifier
+    mockQuotesService = MockQuotesService();
+    sut_quotesNotifier = QuotesNotifier(mockQuotesService);
+  });
+
+  test('Check initial values are correct', () {
+    expect(sut_quotesNotifier.isLoading, false);
+    expect(sut_quotesNotifier.quotes, []);
+  });
+
+  group('getQuotes', () {
+/* Test czy funkcja getQuotes zwraca cytaty z QuotesService 
+   Test jest pisany zgodnie z zasadą AAA (Arrange/Act/Assert)*/
+    test(('getQuotes should call getQuotes function'), () async {
+      /* Arrange -> ustawiamy mockQuotesService tak, aby zwracał pustą listę
+                gdy wywoływana jest funkcja getQuotes */
+      when(() => mockQuotesService.getQuotes()).thenAnswer((_) async => []);
+
+      // Action -> wywołanie funkcji getQuotes
+      await sut_quotesNotifier.getQuotes();
+
+      // Assert -> weryfikacja czy funkcja getQuotes została wywołana raz
+      verify(() => mockQuotesService.getQuotes()).called(1);
+    });
+
+/* Test sprawdzający czy QuotesService jest wywoływany czy nie, oraz czy pola
+są aktualizowane poprawnie i dane są wypełnione */
+    test('''Loading data indicator, 
+  sets quotes, indicates data is not loaded anymore''', () async {
+      when(() => mockQuotesService.getQuotes()).thenAnswer((_) async => [
+            Quotes(1, 'Test Quote 1', 'Test Author 1'),
+            Quotes(2, 'Test Quote 2', 'Test Author 2'),
+            Quotes(
+              3,
+              'Test Quote 3',
+              'Test Author 3',
+            )
+          ]);
+      final future = sut_quotesNotifier.getQuotes();
+      expect(sut_quotesNotifier.isLoading, true);
+      await future;
+      expect(sut_quotesNotifier.quotes, [
+        Quotes(1, 'Test Quote 1', 'Test Author 1'),
+        Quotes(2, 'Test Quote 2', 'Test Author 2'),
+        Quotes(
+          3,
+          'Test Quote 3',
+          'Test Author 3',
+        )
+      ]);
+      expect(sut_quotesNotifier.isLoading, false);
+    });
+
+    /* Powyższy test najpierw ustawia mockQuotesService tak, aby zwracał listę cytatów. 
+      Następnie wywołuje funkcję getQuotes i przechowuje przyszłość w zmiennej. 
+      Następnie sprawdza, czy isLoading jest true. 
+      Następnie czeka na zakończenie przyszłości i sprawdza, czy cytaty są poprawnie wypełnione, a isLoading jest false. */
+  });
+}
