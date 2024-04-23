6fbf620eac03903a97a8867eb88d895703170b63
remik2593 <remik.gagala@droidsonroids.pl>
2024-04-23 14:41:30 +0200

    test

--- integration_test/integration_test.dart
+++ integration_test/integration_test.dart
@@ -5,11 +5,14 @@ import '../test/Widget Tests/reuse_quote_widget_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
+  /* Przed uruchomieniem testu integracyjnego musimy upewnić się, że nasze widżety Flutter są gotowe. 
+  Dlatego musimy wywołać funkcję WidgetsFlutterBinding.ensureInitialized() przed uruchomieniem testu integracyjnego */
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   group('Integration Test', () {
+    // Wywołujemy przekształcony widzet jako funkcję wewnątrz testu integracyjnego
     testWidgets('Login-Page', (tester) => loginWidgetTest(tester));
     testWidgets('All-Quotes-Page', (tester) => quoteWidgetTest(tester));
   });
 }
 
-// testy są odpalane w terminalu komenda: flutter test integration_test/integration_tests.dart
+// testy są odpalane w terminalu komenda: flutter test integration_test/integration_test.dart
--- test/Widget Tests/login_widget_test.dart
+++ test/Widget Tests/login_widget_test.dart
@@ -114,6 +114,9 @@ void main() {
       when(() => mockQuotesService.getQuotes())
           .thenAnswer((_) async => mockQuotesForTesting);
 
+      await expectLater(
+          find.byType(LoginScreen), matchesGoldenFile('login_screen.png'));
+
       await tester.enterText(emailTextField, 'example@wp.pl');
       await tester.enterText(passwordTextField, 'haslo123');
 
